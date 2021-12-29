b0504042da7e9b382337fee9cc5ed1a82ec12860
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2021-12-29 12:34:54 -0300

    WebView

--- Carangas/CarViewController.swift
+++ Carangas/CarViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import WebKit
+import Foundation
 
 class CarViewController: UIViewController {
 
@@ -14,6 +16,8 @@ class CarViewController: UIViewController {
     @IBOutlet weak var lbBrand: UILabel!
     @IBOutlet weak var lbGasType: UILabel!
     @IBOutlet weak var lbPrice: UILabel!
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
     
     // MARK: - Properties
     var car: Car!
@@ -29,6 +33,16 @@ class CarViewController: UIViewController {
         lbGasType.text = car.gas
         lbPrice.text = "R$ \(car.price)"
         title = car.name
+        
+        let name = (title! + "+" + car.brand).replacingOccurrences(of: " ", with: "+")
+        let urlString = "https://www.google.com.br/search?q=\(name)&tbm=isch"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        webView.allowsBackForwardNavigationGestures = true //HABILTA OS GESTURES
+        webView.allowsLinkPreview = true //PRESSIONANDO O LINK CONSEGUIMOS VER O LINK TEMP
+        webView .navigationDelegate = self
+        webView.uiDelegate = self
+        webView.load(request)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -37,4 +51,21 @@ class CarViewController: UIViewController {
     }
     
 
+}
+
+
+@available(iOS 13.0.0, *)
+extension CarViewController: WKNavigationDelegate, WKUIDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loading.stopAnimating()
+        webView.evaluateJavaScript("alert('é isso pessoal')") { returnObject, error in
+            print(returnObject as Any)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async {
+        print(message)
+    }
+    
+    
 }
